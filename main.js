1afb30c97f57e71bf841f096422f581f943d8ea3
one-some <onesomegit@outlook.com>
2025-03-22 11:44:26 -0500

    Smoother

--- main.js
+++ main.js
@@ -2,15 +2,18 @@ const draggingInfo = {
     el: null,
     offsetX: 0,
     offsetY: 0,
+    clientX: 0,
+    clientY: 0,
 };
 
 document.addEventListener("mousedown", function(event) {
     if (event.target.tagName !== "WIN-TITLE") return;
+    event.preventDefault();
 
     draggingInfo.el = event.target.parentElement;
-    draggingInfo.offsetX = null;
-
-    event.preventDefault();
+    const rect = draggingInfo.el.getBoundingClientRect();
+    draggingInfo.offsetX = draggingInfo.clientX - rect.left;
+    draggingInfo.offsetY = draggingInfo.clientY - rect.top;
 });
 
 document.addEventListener("mouseup", function() {
@@ -18,15 +21,17 @@ document.addEventListener("mouseup", function() {
 });
 
 document.addEventListener("mousemove", function(event) {
+    draggingInfo.clientX = event.clientX;
+    draggingInfo.clientY = event.clientY;
+
+    if (!draggingInfo.el) return;
+    window.requestAnimationFrame(anim);
+});
+
+function anim() {
     if (!draggingInfo.el) return;
 
-    if (draggingInfo.offsetX === null) {
-        // New element, need to init offsets.
-        const rect = draggingInfo.el.getBoundingClientRect();
-        draggingInfo.offsetX = event.clientX - rect.left;
-        draggingInfo.offsetY = event.clientY - rect.top;
-    }
+    draggingInfo.el.style.left = `${draggingInfo.clientX - draggingInfo.offsetX}px`;
+    draggingInfo.el.style.top = `${draggingInfo.clientY - draggingInfo.offsetY}px`;
+}
 
-    draggingInfo.el.style.left = `${event.clientX - draggingInfo.offsetX}px`;
-    draggingInfo.el.style.top = `${event.clientY - draggingInfo.offsetY}px`;
-});
